ef108ee7f2b60abe65cb9a8338a83520ac6f4532
Luisglossa <luis.mok@miaglossa.com>
2025-04-28 12:47:41 +0200

    Initial commit

---
+++ api/fetchMonday.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+    const { query } = req.body;
+  
+    const response = await fetch('https://api.monday.com/v2', {
+      method: 'POST',
+      headers: {
+        'Authorization': process.env.MONDAY_API_KEY,
+        'Content-Type': 'application/json',
+        'API-Version': '2023-10'
+      },
+      body: JSON.stringify({ query })
+    });
+  
+    const data = await response.json();
+    res.status(200).json(data);
+  }
+  
